e4c0562235396400a454bd6efb687c1851862b5c
chanhuiseok <chanhuicom@gmail.com>
2020-06-23 00:13:32 +0900

    이벤트 메서드 정의-메서드 바인딩

--- day1/hello-react/src/App.js
+++ day1/hello-react/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Say from './Say';
+import EventPractice from './EventPractice';
 
 
 const App = () => {
   return (
-    <Say/>
+    <EventPractice/>
   );
 };
 
